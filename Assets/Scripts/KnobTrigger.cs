adb6d48b1eba6efb2f6592ff526873093760a309
Jesse-saxion <469397@student.saxion.nl>
2021-03-25 14:01:25 +0100

    Add OculusVRTesting Scene and new script

---
+++ Assets/Scripts/KnobTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class KnobTrigger : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onKnobClick;
+    
+    private void OnTriggerEnter(Collider other) {
+        if(other.isTrigger && OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger)) {
+            onKnobClick?.Invoke();
+        }
+    }
+}
