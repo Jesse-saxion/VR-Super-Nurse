05ebbd1673e6b46cff0329fec6245fa9c26a138e
Jesse <dron20d@mail.ru>
2021-04-14 22:42:50 +0300

    Implemented 3rd step with XR plugin (might need some testing and debugging)

--- Assets/Scripts/StepsHandlers/Step4_Hands.cs
+++ Assets/Scripts/StepsHandlers/Step4_Hands.cs
@@ -1,46 +1,31 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using Valve.VR.InteractionSystem;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 
-//public class Step4_Hands : MonoBehaviour
-//{
-//    public List<Click> objects;
-//    private int index = 0;
-   
-//    public CheckList checkList;
-//    [SerializeField] private AudioClip success;
-//    AudioSource audio;
-//    public float Volume;
-//    public GameObject Tissue;
-//    public GameObject InformPatientScript;
-//    // Start is called before the first frame update
+public class Step4_Hands : MonoBehaviour
+{
+    private int index = 0;
 
-//    private void Start()
-//    {
-//        audio = GetComponent<AudioSource>();       
-//    }
+    public CheckList checkList;
+    [SerializeField] private AudioClip success;
+    AudioSource audio;
+    public float Volume;
+    public GameObject Tissue;
+    public GameObject InformPatientScript;
+    // Start is called before the first frame update
 
-//    public void OnObjectClicked()
-//    {
-//        objects[index].enabled = false;
-//        index++;
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
 
-//        if (index <= objects.Count - 1)
-//        {
-//            objects[index].enabled = true;
-//        }
-//        else
-//        {
-//            Tissue.GetComponent<TriggerNoTissue>().isGlove = true;
-//            InformPatientScript.GetComponent<InformPatientDialog>().alreadyPlayedStep3 = true;
-//            checkList.UpdateCheckList("Put on medical gloves");
-//            // invoke
-//            audio.PlayOneShot(success, Volume);
-          
+    public void OnObjectClicked()
+    {
+        Tissue.GetComponent<TriggerNoTissue>().isGlove = true;
+        InformPatientScript.GetComponent<InformPatientDialog>().alreadyPlayedStep3 = true;
+        checkList.UpdateCheckList("Put on medical gloves");
+        audio.PlayOneShot(success, Volume);
+    }
 
-//        }
-//    }
-    
-//}
+}
