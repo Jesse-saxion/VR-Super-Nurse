dddd16f53eaf525fb4d05a33c086a5b180c2479e
Jesse-saxion <78865254+Jesse-saxion@users.noreply.github.com>
2021-05-20 16:47:09 +0200

    Tube can be watered in kidney dish now

--- Assets/Scripts/StepsHandlers/StepHandler.cs
+++ Assets/Scripts/StepsHandlers/StepHandler.cs
@@ -78,6 +78,10 @@ public class StepHandler : MonoBehaviour
         CompleteStep();
     }
 
+    public bool CheckIfSubstepComplete(int index) {
+        return subStepsList[index-1];
+    }
+
     public void ActivateQuestion(QuestionHandler question)
     {
         if (question == null)
--- Assets/Scripts/Tube/MarkTube.cs
+++ Assets/Scripts/Tube/MarkTube.cs
@@ -6,6 +6,8 @@ public class MarkTube : MonoBehaviour
 {
     public bool canMarkSeveralTimes = false;
     bool canMark = true;
+    [SerializeField]
+    public GameObject tubeStepHandler;
 
     void OnTriggerEnter(Collider other)
     {
@@ -14,11 +16,13 @@ public class MarkTube : MonoBehaviour
             if (other.TryGetComponent<PlaceMark>(out PlaceMark component))
             {
                 other.GetComponent<PlaceMark>().DrawMark();
+                tubeStepHandler.GetComponent<Step6Handler>().TubeMarked();
             }
         }
         else if (other.TryGetComponent<PlaceMark>(out PlaceMark component) && canMark)
         {
             other.GetComponent<PlaceMark>().DrawMark();
+            tubeStepHandler.GetComponent<Step6Handler>().TubeMarked();
             canMark = false;
         }
     }
